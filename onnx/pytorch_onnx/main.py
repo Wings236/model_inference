4d64847266b1158e06910fbac2532ea743c258b5
wings236 <932629613@qq.com>
2025-09-03 17:05:41 +0800

    init onnx pytorch

---
+++ onnx/pytorch_onnx/main.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+import requests
+import numpy as np
+import torch
+from torch import nn
+
+class SuperResolutionNet(nn.Module):
+    def __init__(self, upscale_factor: float) -> None:
+        super().__init__()
+        self.upscale_factor = upscale_factor
+        self.img_upsampler = nn.Upsample(
+            scale_factor=self.upscale_factor,
+            mode="bicubic",
+            align_corners=False
+        )
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv3d(32, 3, kernel_size=5, padding=2)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x: torch.Tensor):
+        x = self.img_upsampler(x)
+        out = self.relu(self.conv1(x))
+        out = self.relu(self.conv2(out))
+        out = self.relu(self.conv3(out))
+
+        return out
+
+
+urls = ['https://download.openmmlab.com/mmediting/restorers/srcnn/srcnn_x4k915_1x16_1000k_div2k_20200608-4186f232.pth',
+    'https://raw.githubusercontent.com/open-mmlab/mmediting/master/tests/data/face/000001.png']
+names = ['srcnn.pth', 'face.png']
+for url, name in zip(urls, names):
+    if not os.path.exists(name):
+        open(name, 'wb').write(requests.get(url).content)
+
