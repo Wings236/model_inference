d8f19884a73ba6bf2a5fa7e68e9eb50bec09b0ff
wings236 <932629613@qq.com>
2025-09-09 15:35:16 +0800

    a simple onnx process

--- onnx/pytorch_onnx/main.py
+++ onnx/pytorch_onnx/main.py
@@ -17,7 +17,7 @@ class SuperResolutionNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
         self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
-        self.conv3 = nn.Conv3d(32, 3, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(32, 3, kernel_size=5, padding=2)
 
         self.relu = nn.ReLU()
 
@@ -36,4 +36,52 @@ names = ['srcnn.pth', 'face.png']
 for url, name in zip(urls, names):
     if not os.path.exists(name):
         open(name, 'wb').write(requests.get(url).content)
+
+def init_torch_model():
+    torch_model = SuperResolutionNet(upscale_factor=3)
+
+    state_dict:dict[str, torch.Tensor] = torch.load("srcnn.pth")['state_dict']
+
+    # Adapt the checkpoint
+    for old_key in list(state_dict.keys()):
+        new_key = ".".join(old_key.split('.')[1:])
+        state_dict[new_key] = state_dict.pop(old_key)
+
+    torch_model.load_state_dict(state_dict)
+    torch_model.eval()
+    return torch_model
+
+
+model = init_torch_model()
+input_img = cv2.imread('face.png').astype(np.float32)
+
+# HWC to NCHW
+input_img = np.transpose(input_img, [2, 0, 1])
+input_img = np.expand_dims(input_img, 0)
+
+# Inference
+torch_output = model(torch.from_numpy(input_img)).detach().numpy()
+
+# NCHW to HWC
+torch_output = np.squeeze(torch_output, 0)
+torch_output = np.clip(torch_output, 0, 255)
+torch_output = np.transpose(torch_output, [1, 2, 0]).astype(np.uint8)
+
+# Show image
+cv2.imwrite("face_torch.png", torch_output)
+
+# Export
+x = torch.randn(1, 3, 256, 256)
+
+with torch.no_grad():
+    torch.onnx.export(
+        model,
+        x,
+        "srcnn.onnx",
+        opset_version=11,
+        input_names=['input'],
+        output_names=['output']
+    )
+
+
 
